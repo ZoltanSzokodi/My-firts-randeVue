f4f214844db72ca022e9f5f340c955fdd8f29d09
Rupert <zoltan.szokodi@hotmail.com>
2019-05-28 21:12:37 +0200

    Initial commit

---
+++ src/components/User.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <h3>The User</h3>
+        <p>My username is {{ username }}</p>
+        <app-info></app-info>
+    </div>
+</template>
+
+<script>
+    import Info from './Info.vue';
+    export default {
+        data() {
+            return {
+                username: 'Rupert'
+            }
+        },
+        components: {
+            'app-info': Info
+        }
+    }
+</script>
+
+<style scoped>
+    p {
+        font-style: italic;
+    }
+</style>
